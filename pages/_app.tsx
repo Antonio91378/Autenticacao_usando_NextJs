f951fd0e60b49f3c9c93583fc19a94ee0cf48840
Antonio <antoniovictor200@gmail.com>
2022-03-28 16:17:40 -0300

    autenticacao com auth0 e github

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import '../styles/style.css'
 import { SessionProvider } from "next-auth/react"
+import type {AppProps} from 'next/app'
 
-export default function MyApp({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
+export default function MyApp({ Component, pageProps: { session, ...pageProps },}:AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
