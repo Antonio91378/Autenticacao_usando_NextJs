12d233d8222c76cb7fc9e4edb7f25833e7cc5050
Antonio <antoniovictor200@gmail.com>
2022-03-28 17:25:25 -0300

    autenticacao com auth0 e github

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -3,11 +3,15 @@ import GithubProvider from "next-auth/providers/github"
 import Auth0Provider from "next-auth/providers/auth0";
 
 export default NextAuth({
+
+  
+
+
     // Configure one or more authentication providers
     providers: [
       GithubProvider({
         clientId: 'Iv1.b73fb80f6eb63fbb',
-        clientSecret: '4b15f31bfd8a0c2136eee44e1891178c9c41f1e6',
+        clientSecret: '563567994aa9c32617f4d0407179304448c53038',
       }),
       Auth0Provider({
         clientId: 'jaoAFEzKr3P6FWQuITUhw9a5wA1kzSFd',
